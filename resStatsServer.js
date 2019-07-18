05d42383f8f3c518a51ba5c55a6e0b80d904e27f
rowlingz <ning_ml@126.com>
2019-07-18 15:46:07 +0800

    获取详细信息

--- resStatsServer.js
+++ resStatsServer.js
@@ -1,29 +1,27 @@
-function getUser(session, oRep) {
-    try {
-        let {user} = JSON.parse(session.res.body);
-        let userData = {
-            fansNum: user.userProfile.ownerCount.fan,
-            followNum:user.userProfile.ownerCount.follow,
-            photoNums: user.userProfile.ownerCount.photo_public,
-            cityName: user.userProfile.cityName,
-            constellation:user.userProfile.constellation,
-            userName: user.userProfile.profile.user_name,
-            verified:user.userProfile.profile.verified,
-            sex: user.userProfile.profile.user_sex,
-            text: user.userProfile.profile.user_text
-        };
+function getFeedData(feedInfo) {
+    let feedData = {
+        userId: feedInfo.user_id,//作者Id
+        pubTime: feedInfo.timestamp,// 发布时间
+        commentCount: feedInfo.comment_count,//评论数
+        likeCount: feedInfo.like_count,//点赞数
+        playCount: feedInfo.view_count,// 浏览量
+        text: feedInfo.caption,//配文
+        coverUrl: feedInfo.cover_thumbnail_urls[1].url,//封面图url
+    };
 
-        let repData = {
-            uid: user.userProfile.profile.use_id,
-            kwaiId: user.userProfile.profile.kwaiId,    //没有的话怎么办？
-            platform: 'KUAISHOU',
-            jabType: 'USER',
-            data: userData,
-            tasks: []
-        };
+    let {photoId} = querystring.parse(feedInfo.share_info);
+    feedData.detailUrl = `http://m.gifshow.com/fw/photo/${photoId}`;
 
-        // todo....
-    } catch (err) {
-        // todo...
-    }  
+    let type = feedInfo.main_mv_urls;
+    if (type) {
+        feedData.feedType = 'mv';
+        feedData.videoUrl = type[1].url;
+    } else {
+        feedData.feedType = 'pictures';
+        let picList = feedInfo.ext_params.atlas.list
+        feedData.pics = picList.map(function (item){
+            return 'http://tx2.a.yximgs.com' + item;
+        });
+    }
+    return feedData;
 }
