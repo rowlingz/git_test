c5548dbc01deae008b40635eae35e7bb3d6645b0
Ning Zhou <ning_ML@126.com>
2019-07-18 16:03:00 +0800

    获取详细信息 (#2)
    * 获取详细信息
    
    * fix(git_test#1): add APPRAISAL analysis

--- resStatsServer.js
+++ resStatsServer.js
@@ -1,29 +1,56 @@
-function getUser(session, oRep) {
+function getFeedData(feedInfo) {
+    let feedData = {
+        userId: feedInfo.user_id,//作者Id
+        pubTime: feedInfo.timestamp,// 发布时间
+        commentCount: feedInfo.comment_count,//评论数
+        likeCount: feedInfo.like_count,//点赞数
+        playCount: feedInfo.view_count,// 浏览量
+        text: feedInfo.caption,//配文
+        coverUrl: feedInfo.cover_thumbnail_urls[1].url,//封面图url
+    };
+
+    let {photoId} = querystring.parse(feedInfo.share_info);
+    feedData.detailUrl = `http://m.gifshow.com/fw/photo/${photoId}`;
+
+    let type = feedInfo.main_mv_urls;
+    if (type) {
+        feedData.feedType = 'mv';
+        feedData.videoUrl = type[1].url;
+    } else {
+        feedData.feedType = 'pictures';
+        let picList = feedInfo.ext_params.atlas.list
+        feedData.pics = picList.map(function (item){
+            return 'http://tx2.a.yximgs.com' + item;
+        });
+    }
+    return feedData;
+}
+
+async function handleAppraisal(session, oRep) {
+    let {photoInfos} = querystring.parse(session.req.body);
+    let feedId = photoInfos.match(/"photoId":"([0-9]+)","/)[1];
+    console.log('feedId: ', feedId);
     try {
-        let {user} = JSON.parse(session.res.body);
-        let userData = {
-            fansNum: user.userProfile.ownerCount.fan,
-            followNum:user.userProfile.ownerCount.follow,
-            photoNums: user.userProfile.ownerCount.photo_public,
-            cityName: user.userProfile.cityName,
-            constellation:user.userProfile.constellation,
-            userName: user.userProfile.profile.user_name,
-            verified:user.userProfile.profile.verified,
-            sex: user.userProfile.profile.user_sex,
-            text: user.userProfile.profile.user_text
-        };
+        let { photos } = JSON.parse(session.res.body);
+        let photoInfo = photos[0];
+        let evaData = getFeedData(photoInfo);
+        evaData.itemId = feedId;
 
-        let repData = {
-            uid: user.userProfile.profile.use_id,
-            kwaiId: user.userProfile.profile.kwaiId,    //没有的话怎么办？
+        let reqData = {
+            uid: `${feedId}|10`,
             platform: 'KUAISHOU',
-            jabType: 'USER',
-            data: userData,
-            tasks: []
+            jobType: 'APPRAISAL',
+            data: evaData
         };
 
-        // todo....
+        console.log('senddata: ', reqData);
+        // logger.info('senddata: ', reqData);
+        await api.finish(reqData);
     } catch (err) {
-        // todo...
-    }  
+        logger.error(err);
+        await api.finish({
+            uid: `${feedId}|10`,
+            isErr: true
+        });
+    }
 }
