0346a374fab3fcdcc9692baed6b6ef66ab3a6fe6
rowlingz <ning_ml@126.com>
2019-07-18 15:55:20 +0800

    fix(git_test#1): add APPRAISAL analysis

--- resStatsServer.js
+++ resStatsServer.js
@@ -25,3 +25,32 @@ function getFeedData(feedInfo) {
     }
     return feedData;
 }
+
+async function handleAppraisal(session, oRep) {
+    let {photoInfos} = querystring.parse(session.req.body);
+    let feedId = photoInfos.match(/"photoId":"([0-9]+)","/)[1];
+    console.log('feedId: ', feedId);
+    try {
+        let { photos } = JSON.parse(session.res.body);
+        let photoInfo = photos[0];
+        let evaData = getFeedData(photoInfo);
+        evaData.itemId = feedId;
+
+        let reqData = {
+            uid: `${feedId}|10`,
+            platform: 'KUAISHOU',
+            jobType: 'APPRAISAL',
+            data: evaData
+        };
+
+        console.log('senddata: ', reqData);
+        // logger.info('senddata: ', reqData);
+        await api.finish(reqData);
+    } catch (err) {
+        logger.error(err);
+        await api.finish({
+            uid: `${feedId}|10`,
+            isErr: true
+        });
+    }
+}
